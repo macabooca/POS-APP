390437ec5f71ab4e362c47c7a332229574ada283
macabooca <macabooca@gmail.com>
2026-09-23 18:01:37 -0700

    Bump cache version to v2
    Updated cache version to ensure new files are fetched.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // bump this version string any time you re-upload index.html so phones fetch the new copy
-const CACHE_NAME = 'pos-app-cache-v1';
+const CACHE_NAME = 'pos-app-cache-v2';
 
 const APP_FILES = [
   './',
